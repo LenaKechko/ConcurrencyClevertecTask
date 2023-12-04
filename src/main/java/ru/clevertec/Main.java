225f3ea3ea66c1251dc44457c60f1334a588f7ba
Elena Kechko <ekechko@gmail.com>
2023-12-04 15:00:12 +0300

    Other variant of task

--- src/main/java/ru/clevertec/Main.java
+++ src/main/java/ru/clevertec/Main.java
@@ -1,5 +1,6 @@
 package ru.clevertec;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.clevertec.entity.Client;
 import ru.clevertec.entity.Server;
 
@@ -8,24 +9,38 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Введите n: ");
 //        Scanner sc = new Scanner(System.in);
 //        int size = sc.nextInt();
-        int size = 10;
+        int size = 6;
         Client client = new Client(size);
-        client.remove(size);
+//        client.requestOfClient();
+//        client.remove(size);
 //        client.print();
-//        ExecutorService executor = Executors.newSingleThreadExecutor();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        IntStream.rangeClosed(1, size)
+                .forEach((i) -> {
+                    client.requestOfClient();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep((long) (Math.random() * (501 - 100) + 100));
+                    } catch (InterruptedException e) {
+                        log.error("Exception!!!!: " + e);
+                    }
+                });
 //        List<Future<Integer>> futures =
 //        IntStream.rangeClosed(1, size)
-//                .forEach((i) -> client.remove());
+//                .forEach((i) -> executor.submit(new Server(client.remove())));
 //                        .toList();
 
+//                IntStream.rangeClosed(1, size)
+//                        .forEach((i) -> executor.submit(new Client(size)));
 //        client.print();
 //        for (Future<Integer> future : futures) {
 //            try {
@@ -42,10 +57,10 @@ public class Main {
 //        executor.shutdown();
 
         System.out.println(client.stop());
+
 //        for (int i = 1; i < size; i++) {
 //            Callable<Integer> callable
         // здесь будут вызываться запросы от клиента на сервер
-//            new Thread(new MyLockClass(i));
         //            new Thread(new Client(i));
 //        }
     }
--- src/main/java/ru/clevertec/entity/Client.java
+++ src/main/java/ru/clevertec/entity/Client.java
@@ -8,7 +8,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 @Slf4j
@@ -17,7 +16,7 @@ public class Client {
     private final List<Integer> listData = new ArrayList<>();
     public static Long accumulator = 0L;
 
-    ExecutorService executor = Executors.newSingleThreadExecutor();
+    ExecutorService executor = Executors.newCachedThreadPool(); // .newFixedThreadPool(2);
 
     public Client(int n) {
         IntStream.rangeClosed(1, n)
@@ -29,20 +28,16 @@ public class Client {
         return listData.remove(index);
     }
 
-    public void remove(int size) {
-        IntStream.rangeClosed(1, size)
-                .forEach((i) -> {
-                    Integer value = getValue();
-                    log.info("Отправка запроса от клиента со значением " + value);
-                    try {
-                        TimeUnit.MILLISECONDS.sleep((long) (Math.random() * (501 - 100) + 100));
-                        Future<Integer> response = executor.submit(new Server(value));
-                        accumulator += response.get();
-                        print();
-                    } catch (InterruptedException | ExecutionException e) {
-                        System.err.println("Exception!!!!: " + e);
-                    }
-                });
+    public void requestOfClient() {
+        Integer value = getValue();
+        log.info("Отправка запроса от клиента со значением " + value);
+        try {
+            Future<Integer> response = executor.submit(new Server(value));
+            accumulator += response.get();
+            print();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Exception!!!!: " + e);
+        }
     }
 
     public void print() {
@@ -54,16 +49,4 @@ public class Client {
         executor.shutdown();
         return accumulator;
     }
-
-//    @Override
-//    public void run() {
-//        int value = (int) (Math.random() * (listData.size()));
-//        Future<Integer> response = remove(value);
-//        try {
-//            accumulator += response.get();
-//            print();
-//        } catch (InterruptedException | ExecutionException e) {
-//            System.err.println("Exception!!!!: " + e);
-//        }
-//    }
 }
